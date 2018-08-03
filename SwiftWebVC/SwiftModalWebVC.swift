5339fd2683be89bb1c84e27d81bac69fc0ac70b2
Hugh Nimmo-Smith <hugh@nimmo-smith.com>
2018-08-04 00:33:06 +0100

    Styling modifications

--- SwiftWebVC/SwiftModalWebVC.swift
+++ SwiftWebVC/SwiftModalWebVC.swift
@@ -10,70 +10,28 @@ import UIKit
 
 public class SwiftModalWebVC: UINavigationController {
     
-    public enum SwiftModalWebVCTheme {
-        case lightBlue, lightBlack, dark
-    }
-    public enum SwiftModalWebVCDismissButtonStyle {
-        case arrow, cross
-    }
-    
     weak var webViewDelegate: UIWebViewDelegate? = nil
     
-    public convenience init(urlString: String, sharingEnabled: Bool = true) {
-        var urlString = urlString
-        if !urlString.hasPrefix("https://") && !urlString.hasPrefix("http://") {
-            urlString = "https://"+urlString
-        }
-        self.init(pageURL: URL(string: urlString)!, sharingEnabled: sharingEnabled)
-    }
-    
-    public convenience init(urlString: String, theme: SwiftModalWebVCTheme, dismissButtonStyle: SwiftModalWebVCDismissButtonStyle, sharingEnabled: Bool = true) {
-        self.init(pageURL: URL(string: urlString)!, theme: theme, dismissButtonStyle: dismissButtonStyle, sharingEnabled: sharingEnabled)
-    }
-    
-    public convenience init(pageURL: URL, sharingEnabled: Bool = true) {
-        self.init(request: URLRequest(url: pageURL), sharingEnabled: sharingEnabled)
-    }
-    
-    public convenience init(pageURL: URL, theme: SwiftModalWebVCTheme, dismissButtonStyle: SwiftModalWebVCDismissButtonStyle, sharingEnabled: Bool = true) {
-        self.init(request: URLRequest(url: pageURL), theme: theme, dismissButtonStyle: dismissButtonStyle, sharingEnabled: sharingEnabled)
-    }
-    
-    public init(request: URLRequest, theme: SwiftModalWebVCTheme = .lightBlue, dismissButtonStyle: SwiftModalWebVCDismissButtonStyle = .arrow, sharingEnabled: Bool = true) {
+    public init(request: URLRequest) {
         let webViewController = SwiftWebVC(aRequest: request)
-        webViewController.sharingEnabled = sharingEnabled
-        webViewController.storedStatusColor = UINavigationBar.appearance().barStyle
         
-        let dismissButtonImageName = (dismissButtonStyle == .arrow) ? "SwiftWebVCDismiss" : "SwiftWebVCDismissAlt"
-        let doneButton = UIBarButtonItem(image: SwiftWebVC.bundledImage(named: dismissButtonImageName),
+        webViewController.storedStatusColor = UINavigationBar.appearance().barStyle
+        let doneButton = UIBarButtonItem(title: "Done",
                                          style: UIBarButtonItemStyle.plain,
                                          target: webViewController,
                                          action: #selector(SwiftWebVC.doneButtonTapped))
-        
-        switch theme {
-        case .lightBlue:
-            doneButton.tintColor = nil
-            webViewController.buttonColor = nil
-            webViewController.titleColor = UIColor.black
-            UINavigationBar.appearance().barStyle = UIBarStyle.default
-        case .lightBlack:
-            doneButton.tintColor = UIColor.darkGray
-            webViewController.buttonColor = UIColor.darkGray
-            webViewController.titleColor = UIColor.black
-            UINavigationBar.appearance().barStyle = UIBarStyle.default
-        case .dark:
-            doneButton.tintColor = UIColor.white
-            webViewController.buttonColor = UIColor.white
-            webViewController.titleColor = UIColor.groupTableViewBackground
-            UINavigationBar.appearance().barStyle = UIBarStyle.black
-        }
-        
+
+        doneButton.tintColor = UIColor.black
+        webViewController.buttonColor = UIColor.black
+        webViewController.titleColor = UIColor.black
+
         if (UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.pad) {
             webViewController.navigationItem.leftBarButtonItem = doneButton
         }
         else {
             webViewController.navigationItem.rightBarButtonItem = doneButton
         }
+        
         super.init(rootViewController: webViewController)
     }
     
